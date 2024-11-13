4c77539c77c38564b728037211bb38b5eddfd3f3
kngval <bkvalteocs@gmail.com>
2024-11-13 20:02:34 +0800

    cp

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.opt.relativenumber = true
+vim.opt.number = true
 vim.g.mapleader = " "
 -- Example for init.lua
 vim.opt.termguicolors = true
